03ab6765e763c6237b918a259b7740f6459840a8
caamittiwari <ca.amittiwary@gmail.com>
2022-12-29 10:21:22 +0530

    anson lsp add inplace of lsp installer

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -16,7 +16,7 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.pylint,
+    diagnostics.pylint,
 		formatting.google_java_format,
     formatting.sql_formatter,
 	},
